d2667ca5f7d1e7b1e17d314ab0f7cb055d5ae22d
Justin James <justinmjames@protonmail.com>
2022-06-18 15:22:52 -0700

    commit tree 7b108f16387500845255f73ba4d529dfe807f2e2

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++){
+    const sumComparable = (target - array[i])
+  for(let j = i + 1; j < array.length; j++)
+    if(array[j] === sumComparable){
+      return true
+    
+  }
+  }
+  return false
 }
+
+
 
 /* 
   Write the Big O time complexity of your function here
+  0(n)
 */
 
 /* 
-  Add your pseudocode here
+  write a function that takes two arguments. The arguments will be an array and a single number. If any pair 
+  of numbers in the array add up to the single number the function will return true.  
+Add your pseudocode here
+      add the first number to each number in the array
+      compare the sum to the target number
+      return true when the sum and target match
+      else return false 
+
 */
 
 /*
   Add written explanation of your solution here
+  this function takes two arguments, an array and a target number. A for loop iterates through the array one time 
+  and subtracts each number in the array from the target number. The value of each subtracted number is stored in 
+  the sumComparable value. A second for loop iterates through the array and compares
+  each array value to the the values stored in sumComparable. If a value in the array absoblutely equals one of 
+  the numbers in sumComparable the function will return true.  
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +52,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([10, 12, 40], 15));
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([1], 0));
 }
 
 module.exports = hasTargetSum;
